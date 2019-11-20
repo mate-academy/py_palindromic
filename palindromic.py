d7098100c2a697beff0df4168db13f5b0f3a0633
vlysenok <tina.lysenok@gmail.com>
2019-11-20 14:04:33 +0200

    added hw3_palindromic

--- palindromic.py
+++ palindromic.py
@@ -1,3 +1,21 @@
-def get_longest_palindrome(s: str) -> str:
+"""
+module docstring
+"""
+import itertools
 
-    return "x"
+
+def get_longest_palindrome(strng: str) -> str:
+    """
+    Find the longest substring which is palindrome for a given string.
+    :param strng: str
+    :return: str
+    """
+    max_pol_strng = ''
+    max_pol_strng_len = 0
+
+    for start, stop in itertools.combinations(range(len(strng) + 1), 2):
+        sub_strng = strng[start:stop]
+        if (len(sub_strng) > max_pol_strng_len) and (sub_strng == sub_strng[::-1]):
+            max_pol_strng, max_pol_strng_len = sub_strng, len(sub_strng)
+
+    return max_pol_strng
