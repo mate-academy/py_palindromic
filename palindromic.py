68362a3a827ff7c9e4f451501943f29e7dd61866
logotip <logotip123@yahoo.com>
2019-11-20 12:55:29 +0200

    v 1.0

--- palindromic.py
+++ palindromic.py
@@ -1,3 +1,14 @@
-def get_longest_palindrome(s: str) -> str:
+""""Palindromic search module"""
 
-    return "x"
+
+def get_longest_palindrome(text: str) -> str:
+    """"Palindromic search function"""
+    size = len(text)
+    result = ""
+    for index in range(size):
+        for second_index in range(index+1, size+1):
+            temp = text[index:second_index]
+            if temp == temp[::-1]:
+                if len(temp) > len(result):
+                    result = temp
+    return result
