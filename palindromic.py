f5b0a422a8d90d7dc65ce09be9f1ea9017412e2e
Oleh_M <morhaliuk@gmail.com>
2019-11-20 13:33:21 +0200

    Solved

--- palindromic.py
+++ palindromic.py
@@ -1,3 +1,19 @@
-def get_longest_palindrome(s: str) -> str:
+"""This module contains function
+get_longest_palindrome(s)
+"""
 
-    return "x"
+def get_longest_palindrome(string: str) -> str:
+    """Returns a substring of a given string
+    which is a palindrome"""
+    length_s = len(string)
+
+    if length_s < 2:
+        return string
+    longest_pal = ''
+    for s_pal in range(0, length_s - 1):
+        for e_pal in range(s_pal + 1, length_s):
+            substr = string[s_pal:e_pal + 1]
+            if substr == substr[::-1] and len(substr) > len(longest_pal):
+                longest_pal = substr
+
+    return longest_pal if longest_pal else string[0]
