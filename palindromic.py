e42914cda61f289466877ed632a21d2663716796
d1makov <makovskyj.danylo@gmail.com>
2019-11-20 14:14:39 +0200

    revised

--- palindromic.py
+++ palindromic.py
@@ -1,3 +1,23 @@
-def get_longest_palindrome(s: str) -> str:
+"""
+docstring
+"""
 
-    return "x"
+
+def get_longest_palindrome(string: str) -> str:
+    """
+
+    :param string:
+    :return:
+    """
+    if reverse(string) == string:
+        return string
+
+
+def reverse(string):
+    """
+
+    :param string:
+    :return:
+    """
+    return string[::-1]
+
