75dedb3ab54ebaf5b4d2ae7869061e7337d64f5a
Artem <artger@ukr.net>
2019-11-20 14:21:19 +0200

    all test passed

--- palindromic.py
+++ palindromic.py
@@ -1,3 +1,28 @@
-def get_longest_palindrome(s: str) -> str:
+'''
+Vodule
+'''
 
-    return "x"
+
+def is_palindrom(strng: str) -> bool:
+    '''
+
+    :param strng:
+    :return:
+    '''
+    return strng == strng[::-1]
+
+
+def get_longest_palindrome(strng: str) -> str:
+    '''
+
+    :param s:
+    :return:
+    '''
+    lngth = len(strng)
+    for i in range(lngth - 1):
+        if is_palindrom(strng[i:]):
+            return strng[i:]
+    for i in range(lngth, 0, -1):
+        if is_palindrom(strng[:i]):
+            return strng[:i]
+    return ''
