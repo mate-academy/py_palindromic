73c45bdf4819362e5b997d45c9825d02fab1672f
Platoniuks <platon@alterpost.org>
2019-11-21 08:37:54 +0200

    get the longest palindrome

--- palindromic.py
+++ palindromic.py
@@ -1,3 +1,14 @@
-def get_longest_palindrome(s: str) -> str:
+"""Find the longest palindrome"""
 
-    return "x"
+
+def get_longest_palindrome(string: str) -> str:
+    """Find the longest palindrome"""
+    longest = ''
+    reversed_string = string[::-1]
+    for index, _value in enumerate(string, start=1):
+        if string[:index] == string[:index][::-1] and index > len(longest):
+            longest = string[:index]
+    for index, _value in enumerate(reversed_string, start=1):
+        if reversed_string[:index] == reversed_string[:index][::-1] and index > len(longest):
+            longest = reversed_string[:index]
+    return longest
