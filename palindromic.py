b8917ab7db5c196cba4518c9948e96947778c5aa
Andrii <andrewsotnikov88@gmail.com>
2019-11-22 17:50:52 +0200

    Palindrome solution

--- palindromic.py
+++ palindromic.py
@@ -1,3 +1,11 @@
-def get_longest_palindrome(s: str) -> str:
-
-    return "x"
+def get_longest_palindrome(phrase: str) -> str:
+    result = []
+    if phrase:
+        for index, value in enumerate(phrase):
+            for j in range(0, index + 1):
+                sub_phrase = phrase[j:index + 1]
+                if sub_phrase == sub_phrase[::-1]:
+                    result.append(sub_phrase)
+        return max(result, key=len)
+    else:
+        return ''
