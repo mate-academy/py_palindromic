5c6e2a79468039cfdc8ec98fae96ca816af3fc3f
YuPo13 <yuliya.povzlo@gmail.com>
2019-11-22 11:35:45 +0200

    Finally done

--- palindromic.py
+++ palindromic.py
@@ -1,3 +1,28 @@
-def get_longest_palindrome(s: str) -> str:
+"""module docstring"""
 
-    return "x"
+def find_palindrome_substr(some_string, left, right, max_string):
+    """function docstring"""
+    while left >= 0 and right < len(some_string):
+        if some_string[left] != some_string[right]:
+            break
+        pal = some_string[left:right + 1]
+        if len(pal) > len(max_string):
+            max_string = pal
+        left -= 1  # expand to left
+        right += 1  # expand to right
+    return max_string
+
+def get_longest_palindrome(some_string):
+    """function docstring"""
+    max_string = ""
+    if len(some_string) != 0:
+        for seq in range(len(some_string)):
+            max_string = find_palindrome_substr(some_string, seq, seq + 1, max_string)
+            max_string1 = find_palindrome_substr(some_string, seq - 1, seq + 1, max_string)
+            if max_string < max_string1:
+                max_string = max_string1
+
+        if max_string == "":
+            max_string += some_string[0]
+
+    return max_string
