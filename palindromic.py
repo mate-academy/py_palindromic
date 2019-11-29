c8ec46fbdf600ce8a4708f871dcb16ef64781303
koreyo <KoretsValentyn@gmail.com>
2019-11-29 15:14:50 +0200

    hw

--- palindromic.py
+++ palindromic.py
@@ -1,3 +1,21 @@
-def get_longest_palindrome(s: str) -> str:
+""" get sub palindrome"""
 
-    return "x"
+
+def get_longest_palindrome(word: str) -> str:
+    """get long palindrome"""
+    count = 0
+    if word == "":
+        return ""
+
+    longest = word[0]
+    sub = [word[i:j] for i in range(len(word)) for j in range(i + 1, len(word) + 1)]
+    for i in sub:
+        if len(i) > 1 and len(i) > count and check_pl(i):
+            count = len(i)
+            longest = i
+    return longest
+
+
+def check_pl(wrd):
+    """check word"""
+    return wrd == wrd[::-1]
